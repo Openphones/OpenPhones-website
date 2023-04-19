3396f066c0e9423cdef07948d4800618702885e7
noClaps <github@zerolimits.dev>
2023-04-19 23:18:21 +0100

    Remove astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
-import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://Openphones.github.io",
   base: "/OpenPhones-website",
-  integrations: [sitemap(), compress()]
+  integrations: [sitemap()]
 });
