25bc1dca49a003b54537a27372e6f906316788f2
noClaps <github@zerolimits.dev>
2023-04-24 23:11:20 +0100

    Revert to using local file for now

--- src/scripts/dataIO.ts
+++ src/scripts/dataIO.ts
@@ -1,6 +1,6 @@
 var currency = sessionStorage.getItem("currency") ? sessionStorage.getItem("currency")?.toString() : "USD";
 
-var response = await fetch(`https://mester.info:9876/products?currency=${currency}`, { mode: "no-cors" });
+var response = await fetch("/OpenPhones-website/phones.json");
 export var jsonLinks = await response.json();
 
 export function currencyConvert(value: number) {
