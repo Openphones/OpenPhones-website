3f22624667d58e891ce1fac56f513b3d6eb8cbe1
noClaps <github@zerolimits.dev>
2023-04-30 23:41:48 +0100

    Use new API URL

--- src/scripts/dataIO.ts
+++ src/scripts/dataIO.ts
@@ -1,6 +1,6 @@
 var currency = sessionStorage.getItem("currency") ? sessionStorage.getItem("currency")?.toString() : "USD";
 
-var response = await fetch(`https://openphones.global:5000/products?currency=${currency}`);
+var response = await fetch(`https://api.openphones.global/products?currency=${currency}`);
 export var jsonLinks = await response.json();
 
 export function currencyConvert(value: number) {
