528fdda1aa69784f3f169b5fda5852b7d816d447
noClaps <github@zerolimits.dev>
2023-04-30 22:18:23 +0100

    Use hosted API to fetch items

--- src/scripts/dataIO.ts
+++ src/scripts/dataIO.ts
@@ -1,6 +1,6 @@
 var currency = sessionStorage.getItem("currency") ? sessionStorage.getItem("currency")?.toString() : "USD";
 
-var response = await fetch("/OpenPhones-website/phones.json");
+var response = await fetch(`https://openphones.global:5000/products?currency=${currency}`);
 export var jsonLinks = await response.json();
 
 export function currencyConvert(value: number) {
