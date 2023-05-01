b875c96b25c32fb34e9757a307aa44eecaffc186
noClaps <github@zerolimits.dev>
2023-05-01 20:09:59 +0100

    Use onion site for backend if using Tor

--- src/scripts/dataIO.ts
+++ src/scripts/dataIO.ts
@@ -1,6 +1,8 @@
 var currency = sessionStorage.getItem("currency") ? sessionStorage.getItem("currency")?.toString() : "USD";
 
-var response = await fetch(`https://api.openphones.global/products?currency=${currency}`);
+var url = location.origin.endsWith(".onion") ? "http://nrnd5rs5aut37ny3vst42sj7a3v7xpw2y42k453gf6ibjlro5yjdpvyd.onion" : "https://api.openphones.global"
+
+var response = await fetch(`${url}/products?currency=${currency}`);
 export var jsonLinks = await response.json();
 
 export function currencyConvert(value: number) {
